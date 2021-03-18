23393a7d854f49614ff7426dddc37cb8c29e0545
Aleksandr Rykalin <arykalin@gmail.com>
2021-03-18 13:24:39 +0300

    adding telegram messenger

--- telegram/telegram.go
+++ telegram/telegram.go
@@ -6,20 +6,33 @@ import (
 	"github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
-func SendMessage(chatID int64, text string) error {
-	bot, err := tgbotapi.NewBotAPI("token")
-	if err != nil {
-		log.Panic(err)
-	}
-	bot.Debug = true
+type teleLog struct {
+	chatID int64
+	bot    *tgbotapi.BotAPI
+}
 
-	log.Printf("Authorized on account %s", bot.Self.UserName)
-	msg := tgbotapi.NewMessage(chatID, text)
-	//msg.ReplyToMessageID = update.Message.MessageID
+type TeleLog interface {
+	SendMessage(text string) error
+}
 
-	_, err = bot.Send(msg)
+func (t *teleLog) SendMessage(text string) error {
+	msg := tgbotapi.NewMessage(t.chatID, text)
+	_, err := t.bot.Send(msg)
 	if err != nil {
 		return err
 	}
 	return nil
+}
+
+func NewTelegramLog(chatID int64, token string) TeleLog {
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Panic(err)
+	}
+	bot.Debug = true
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+	return &teleLog{
+		chatID: chatID,
+		bot:    bot,
+	}
 }
